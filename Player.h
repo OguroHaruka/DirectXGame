4ca00da21376212ca612161e10f471bd214bc636
Oguro Haruka <Oguro Haruka@MSI>
2023-04-24 10:43:07 +0900

    02_01

---
+++ Player.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "WorldTransform.h"
+#include"Model.h"
+
+class Player {
+public:
+	void Initialize(Model*model,uint32_t textureHandle);
+	void Update();
+	void Draw(ViewProjection& viewProjection);
+
+private:
+	WorldTransform worldTransform_;
+	Model* model_ = nullptr;
+	uint32_t textureHandle_=0u;
+	Player* player_ = nullptr;
+};
--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -4,16 +4,32 @@
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() { 
+	delete model_;
+	delete player_;
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+
+	textureHandle_ = TextureManager::Load("DRA.png");
+	model_ = Model::Create();
+
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
+
+	player_ = new Player();
+
+	player_->Initialize(model_,textureHandle_);
 }
 
-void GameScene::Update() {}
+void GameScene::Update() { 
+	player_->Update();
+
+}
 
 void GameScene::Draw() {
 
@@ -40,6 +56,9 @@ void GameScene::Draw() {
 
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
+	
+	player_->Draw(viewProjection_);
+
 	/// </summary>
 
 	// 3Dオブジェクト描画後処理
