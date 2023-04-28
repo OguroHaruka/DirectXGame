8bfc2b73b2b77fccb946a459fa30e92850fd9632
Oguro Haruka <Oguro Haruka@MSI>
2023-04-28 14:18:41 +0900

    02_02

--- Player.cpp
+++ Player.cpp
@@ -6,9 +6,28 @@ void Player::Initialize(Model* model, uint32_t textureHandle) {
 	model_ = model;
 	textureHandle_ = textureHandle;
 	worldTransform_.Initialize();
+	input_ = Input::GetInstance();
+	//worldTransform_.scale_ = {5.0f, 1.0f, 1.0f};
+	//Matrix4x4 matScale;
 }
 
-void Player::Update() { worldTransform_.TransferMatrix(); }
+/* void Player::Update() { 
+	worldTransform_.TransferMatrix(); 
+	Vector3 move = {0, 0, 0};
+
+	const float kCharacterSpeed = 0.2;
+	if (input_->PushKey(DIK_LEFT)) {
+
+		move.x -= kCharacterSpeed;
+	} else if (input_->PushKey(DIK_RIGHT)) {
+		move.x += kCharacterSpeed;
+	} else if (input_->PushKey(DIK_UP)) {
+		move.y -= kCharacterSpeed;
+	} else if (input_->PushKey(DIK_DOWN)) {
+		move.y += kCharacterSpeed;
+	}
+
+}*/
 
 void Player::Draw(ViewProjection& viewProjection) {
 	model_->Draw(worldTransform_, viewProjection, textureHandle_);
--- Player.h
+++ Player.h
@@ -1,16 +1,19 @@
 ﻿#pragma once
 #include "WorldTransform.h"
 #include"Model.h"
+#include"Input.h"
 
 class Player {
 public:
 	void Initialize(Model*model,uint32_t textureHandle);
 	void Update();
 	void Draw(ViewProjection& viewProjection);
+	
 
 private:
 	WorldTransform worldTransform_;
 	Model* model_ = nullptr;
 	uint32_t textureHandle_=0u;
 	Player* player_ = nullptr;
+	Input* input_=nullptr;
 };
