a709e7c02604dcc9f3d92c493b4ee96869db684c
Oguro Haruka <Oguro Haruka@MSI>
2023-04-28 15:48:09 +0900

    02_02

--- Player.cpp
+++ Player.cpp
@@ -5,15 +5,21 @@ void Player::Initialize(Model* model, uint32_t textureHandle) {
 	assert(model);
 	model_ = model;
 	textureHandle_ = textureHandle;
-	worldTransform_.Initialize();
 	input_ = Input::GetInstance();
-	//worldTransform_.scale_ = {5.0f, 1.0f, 1.0f};
-	//Matrix4x4 matScale;
+	worldTransform_.scale_ = {5.0f, 1.0f, 1.0f};
+	worldTransform_.rotation_ = {0.0f, 0.0f, 0.0f};
+	worldTransform_.Initialize();
 }
 
-/* void Player::Update() { 
+void Player::Update() { 
 	worldTransform_.TransferMatrix(); 
 	Vector3 move = {0, 0, 0};
+	Matrix4x4 matScale;
+	matScale.m[0][0] = worldTransform_.scale_.x;
+	matScale.m[1][1] = worldTransform_.scale_.y;
+	matScale.m[2][2] = worldTransform_.scale_.z;
+	matScale.m[3][3] = 1;
+	worldTransform_.TransferMatrix();
 
 	const float kCharacterSpeed = 0.2;
 	if (input_->PushKey(DIK_LEFT)) {
@@ -27,7 +33,7 @@ void Player::Initialize(Model* model, uint32_t textureHandle) {
 		move.y += kCharacterSpeed;
 	}
 
-}*/
+}
 
 void Player::Draw(ViewProjection& viewProjection) {
 	model_->Draw(worldTransform_, viewProjection, textureHandle_);
